b65a38c0026f02b41c5ffffe8cea2e11eed7f19e
Justin Delille <justind7513@gmail.com>
2023-06-09 13:18:21 -0700

    feat: added remove like functionality

--- src/app/components/posts/post-card/post-card-footer/PostCardFooter.tsx
+++ src/app/components/posts/post-card/post-card-footer/PostCardFooter.tsx
@@ -1,7 +1,6 @@
-
-import { FaReply } from 'react-icons/fa'
-import { AiTwotoneLike } from 'react-icons/ai'
-import { useRouter } from 'next/navigation'
+import { FaReply } from 'react-icons/fa';
+import { AiTwotoneLike } from 'react-icons/ai';
+import { useRouter } from 'next/navigation';
 import styles from './PostCardFooter.module.scss';
 import { BsFillBookmarkFill, BsShareFill, BsThreeDots } from 'react-icons/bs';
 import { useCallback, useState } from 'react';
@@ -15,37 +14,43 @@ type Props = {
   postId: string;
   currentUser: SafeUser | null;
   post: any;
-}
+};
 
 const PostCardFooter: React.FC<Props> = ({ postId, currentUser, post }) => {
-
   const router = useRouter();
-  const isBookmarked = currentUser?.bookmarks.includes(postId)
-  const hasLiked = post.likedIds.includes(currentUser?.id)
+  const isBookmarked = currentUser?.bookmarks.includes(postId);
 
+  const [bookmark, setBookmark] = useState(isBookmarked);
 
-  const [bookmark, setBookmark] = useState(isBookmarked)
-  const [like, setLike] = useState(hasLiked)
-  const [localLike, setLocalLike] = useState(false || hasLiked)
-  const [localLikeCount, setLocalLikeCount] = useState(0 || post.likedIds.length)
+  const [localLikeCount, setLocalLikeCount] = useState(
+    0 || post.likedIds.length
+  );
+
+  const likeSet = new Set(post.likedIds);
+  const hasLiked = () => {
+    return likeSet.has(currentUser?.id as string);
+  }
+
+  const [like, setLike] = useState(hasLiked);
+  const [localLike, setLocalLike] = useState(false || hasLiked);
+
 
 
   const onBookmark = useCallback(() => {
     setBookmark(true);
-    axios.post(`/api/bookmark/${postId}`)
+    axios
+      .post(`/api/bookmark/${postId}`)
       .then(() => {
         router.refresh();
       })
       .catch((error) => {
-        console.log(error)
+        console.log(error);
         setBookmark(false);
-      })
-  }, [postId, router])
+      });
+  }, [postId, router]);
 
   const onLike = useCallback(
     (id: string) => {
-
-
       if (!currentUser?.id) {
         return;
       }
@@ -59,13 +64,30 @@ const PostCardFooter: React.FC<Props> = ({ postId, currentUser, post }) => {
           router.refresh();
         })
         .catch((error) => {
-          console.log(error)
+          console.log(error);
           setLocalLikeCount((prevLikeCount: number) => prevLikeCount - 1);
-          setLocalLike(false)
-        })
-
+          setLocalLike(false);
+        });
     },
     [currentUser?.id, router]
+  );
+
+  const onRemoveLike = useCallback(
+    (id: string) => {
+      setLocalLikeCount((prevLikeCount: number) => prevLikeCount - 1);
+      setLocalLike(false);
+
+      axios
+        .delete(`/api/like-post/${id}`)
+        .then(() => {
+          router.refresh();
+        })
+        .catch(() => {
+          setLocalLikeCount((prevLikeCount: number) => prevLikeCount + 1);
+          setLocalLike(true);
+        });
+    },
+    [router]
   );
 
   const openPostPreview = (post: Post) => {
@@ -74,28 +96,35 @@ const PostCardFooter: React.FC<Props> = ({ postId, currentUser, post }) => {
     postPreviewStore.setPost(post);
   };
 
-
   return (
     <div className={styles.postCardFooter}>
-      <div className={styles.icon} onClick={() => openPostPreview(post)} >
+      <div className={styles.icon} onClick={() => openPostPreview(post)}>
         <FaReply size={15} color='#5E616F' />
         <span>{post.comments.length || 0}</span>
       </div>
-      <div className={styles.icon} onClick={() => onLike(postId)}>
-        <AiTwotoneLike size={15} color='#5E616F' className={localLike ? styles.liked : ""} />
+      <div className={styles.icon} onClick={() => localLike ? onRemoveLike(postId) : onLike(postId)}>
+        <AiTwotoneLike
+          size={15}
+          color='#5E616F'
+          className={localLike ? styles.liked : ''}
+        />
         <span>{localLikeCount || 0}</span>
       </div>
       <div className={styles.icon} onClick={() => onBookmark()}>
-        <BsFillBookmarkFill size={15} color='#5E616F' className={bookmark ? styles.bookmarked : ""} />
+        <BsFillBookmarkFill
+          size={15}
+          color='#5E616F'
+          className={bookmark ? styles.bookmarked : ''}
+        />
       </div>
-      <div className={styles.icon} >
+      <div className={styles.icon}>
         <BsShareFill size={15} color='#5E616F' />
       </div>
       <div className={styles.icon}>
         <BsThreeDots size={15} color='#5E616F' />
       </div>
-    </div >
+    </div>
   );
-}
+};
 
 export default PostCardFooter;
