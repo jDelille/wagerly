110012ea54b9620287b1a8121c164b918acfdf81
Justin Delille <justind7513@gmail.com>
2023-06-23 00:58:59 -0700

    fix: added scrollable gamebar

--- src/app/sportsbook/page.tsx
+++ src/app/sportsbook/page.tsx
@@ -3,23 +3,35 @@ import FeedHeader from '../components/feed/feed-header/FeedHeader';
 import PostSkeleton from '../components/skeleton/post-skeleton/PostSkeleton';
 
 import styles from './Page.module.scss';
+import getCurrentUser from '../actions/getCurrentUser';
 
+const DynamicSportsbookGames = dynamic(
+ () => import('../components/sportsbook/sportsbook-games/SportsbookGames'),
+ {
+  loading: () => (
+   <>
+    <PostSkeleton />
+    <PostSkeleton />
+    <PostSkeleton />
+   </>
+  ),
+ }
+);
 
-const DynamicSportsbookGames = dynamic(() => import('../components/sportsbook/sportsbook-games/SportsbookGames'), {
- loading: () => <>
-  <PostSkeleton />
-  <PostSkeleton />
-  <PostSkeleton />
- </>
-})
+const Sportsbook = async () => {
+ const currentUser = await getCurrentUser();
 
-const Sportsbook = () => {
  return (
   <div className={styles.main}>
-   <FeedHeader label='Sportsbook' isBack={false} />
+   <FeedHeader
+    label='Sportsbook'
+    isBack={false}
+    currentUserPhoto={currentUser?.photo as string}
+    currentUsername={currentUser?.username as string}
+   />
    <DynamicSportsbookGames />
   </div>
  );
-}
+};
 
 export default Sportsbook;
