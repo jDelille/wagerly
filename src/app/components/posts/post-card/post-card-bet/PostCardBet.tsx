15283ab8de2eb14a1b397e3486e9c7347ab067a7
Justin Delille <justind7513@gmail.com>
2023-06-27 12:41:34 -0700

    updated bet card style

--- src/app/components/posts/post-card/post-card-bet/PostCardBet.tsx
+++ src/app/components/posts/post-card/post-card-bet/PostCardBet.tsx
@@ -9,37 +9,43 @@ type Props = {
 }
 
 
-const PostCardBet: React.FC<Props> = ({ post: bet }) => {
+const PostCardBet: React.FC<Props> = ({ post: Bet }) => {
+
+ const { logo, name, type, bet, matchup, wager, payout, odds } = Bet
+
+ const moneyline = type === 'To win'
 
  return (
   <div className={styles.betContainer}>
-   <div className={styles.bet}>
-    <div className={styles.displayName}>
-     <Image src={bet.logo} alt='logo' width={20} height={20} />
-     <strong>{bet.name}
-     </strong>
+   <div className={styles.top}>
+    <div className={styles.bet}>
+     <div className={styles.logo}>
+      <Image src={logo} alt='logo' width={28} height={28} style={{ objectFit: 'contain' }} />
+     </div>
+     <div className={styles.betInfo}>
+      <strong>{name} &bull; {type}</strong>
+      <span>{matchup}</span>
+     </div>
     </div>
-    <div className={styles.type}>
-     <span>{bet.type}</span>
-     <strong>{bet.bet}</strong>
+    <div className={styles.odds}>
+     <strong>{moneyline ? 'ML' : bet}</strong>
+     <span>{odds}</span>
     </div>
-    <div className={styles.matchup}>
-     <span>{bet.matchup}</span>
-    </div>
+   </div>
+
+   <div className={styles.bottom}>
     <div className={styles.wager}>
-     <span>Wagered: <strong>${bet.wager}</strong></span>
-     <span>Payout: <strong>${bet.payout}</strong></span>
+     <span>Wagered ${wager}</span>
     </div>
-
-    <p className={styles.odds}>
-     {bet.odds}
-    </p>
-    {/* <Link href={`/sportsbook/${bet.description}`} className={styles.description}>View matchup</Link > */}
+    <div className={styles.payout}>
+     <span>Net Payout ${payout}</span>
+    </div>
    </div>
 
-   <div className={styles.disclaimer}>
+
+   {/* <div className={styles.disclaimer}>
     <p>Odds shown are at time of post and are subject to change.</p>
-   </div>
+   </div> */}
   </div>
  );
 }
